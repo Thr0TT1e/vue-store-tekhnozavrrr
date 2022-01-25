84b48e682a4f56e68f6d0b165ece02fed5792429
Thr0TT1e <7hr0771e@gmail.com>
2022-01-25 22:19:51 +0300

    First commit

---
+++ src/file_1.js
@@ -0,0 +1,7 @@
+const text1 = 'Текст, который надо вывести',
+      text2 = 'Вы не поверите )))'
+
+export default {
+  text1,
+  text2
+}
---
+++ src/file_2.js
@@ -0,0 +1,3 @@
+export const stdout = text => {
+  alert(text)
+}
