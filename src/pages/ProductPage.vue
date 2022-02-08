eecbc4ec5fb74149a8206dfd59cef9ff2602115b
Artem Lobasov <a.lobasov@virtlabs.ru>
2022-02-08 11:59:15 +0300

    Добавлены картинки в продукте и исправлена подгрузка css

--- src/pages/ProductPage.vue
+++ src/pages/ProductPage.vue
@@ -37,25 +37,25 @@
         <ul class="pics__list">
           <li class="pics__item">
             <a href="" class="pics__link pics__link--current">
-              <img width="98" height="98" src="img/phone-square-1.jpg" srcset="img/phone-square-1@2x.jpg 2x"
+              <img width="98" height="98" :src="product.image" srcset="img/phone-square-1@2x.jpg 2x"
                    alt="Название товара">
             </a>
           </li>
           <li class="pics__item">
             <a href="" class="pics__link">
-              <img width="98" height="98" src="img/phone-square-2.jpg" srcset="img/phone-square-2@2x.jpg 2x"
+              <img width="98" height="98" :src="product.image" srcset="img/phone-square-2@2x.jpg 2x"
                    alt="Название товара">
             </a>
           </li>
           <li class="pics__item">
             <a href="" class="pics__link">
-              <img width="98" height="98" src="img/phone-square-3.jpg" srcset="img/phone-square-3@2x.jpg 2x"
+              <img width="98" height="98" :src="product.image" srcset="img/phone-square-3@2x.jpg 2x"
                    alt="Название товара">
             </a>
           </li>
           <li class="pics__item">
             <a class="pics__link" href="#">
-              <img width="98" height="98" src="img/phone-square-4.jpg" srcset="img/phone-square-4@2x.jpg 2x"
+              <img width="98" height="98" :src="product.image" srcset="img/phone-square-4@2x.jpg 2x"
                    alt="Название товара">
             </a>
           </li>
